a3adb54b0bb1c96c76d5de6081b19174ad4375f6
nmnmj <nmnjay@gmail.com>
2023-05-17 10:23:36 +0530

    x

--- models/otpModel.js
+++ models/otpModel.js
@@ -4,7 +4,7 @@ const otpSchema =  mongoose.Schema({
     otp: {type: Number, trim: true, required: true},
     email: {type: String, trim: true, required: true},
     time: { type: Date, default: Date.now },
-    otime: { type: Date, default: Date.now }
+    otime: { type: Date, required: true, default: Date.now }
 })
 
 const otpModel = mongoose.model("otp", otpSchema)
